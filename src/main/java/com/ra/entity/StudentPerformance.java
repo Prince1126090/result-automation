7b64bdd83f244f0a04b2ba70e51a48a3ff85bbf0
Zahurul Islam Prince <prince1126090@gmail.com>
2020-07-10 16:54:06 +0600

    add studentPerformance model

---
+++ src/main/java/com/ra/entity/StudentPerformance.java
@@ -0,0 +1,41 @@
+package com.ra.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@Table(name = "student_performance")
+public class StudentPerformance extends BaseEntity {
+
+    @NotNull
+    private Integer year;
+    @NotNull
+    private Integer studentClass;
+    @NotBlank
+    private String section;
+    @NotNull
+    private Integer roll;
+
+    @NotBlank
+    private String exam;
+
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
+    private String marks;
+
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
+    private String others;
+
+}
