95863296b1f6f7f90873b14b0fd18851a770130c
Zahurul Islam Prince <prince1126090@gmail.com>
2020-07-25 20:40:56 +0600

    add join column

--- src/main/java/com/ra/entity/Mark.java
+++ src/main/java/com/ra/entity/Mark.java
@@ -6,10 +6,7 @@ import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
@@ -21,6 +18,7 @@ import java.math.BigDecimal;
 public class Mark extends BaseEntity {
 
     @ManyToOne
+    @JoinColumn(name="student_performance_id")
     private StudentPerformance studentPerformance;
 
     @NotBlank
